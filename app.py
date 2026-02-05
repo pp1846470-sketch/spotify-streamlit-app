771e68a3737b60d236d4b319e1d04d756a8c93bb
pp1846470-sketch <pp1846470@gmail.com>
2026-02-05 09:41:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -194,7 +194,7 @@ elif page == "Model Performance":
     X["album_type"] = X["album_type"].apply(lambda x: 1 if x == "single" else 0)
     X["explicit"] = X["explicit"].apply(lambda x: 1 if x == True else 0)
     st.write(X.describe())
-    print(X.head())
+    st.write(X.head())
     y = df["popular"]
     X.describe()
     # y_pred = model.predict(X)
